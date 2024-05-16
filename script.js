7042a0ccdc6ceecad9608e743bcb7449df1f106d
MadinaAmangeldina <161343110+MadinaAmangeldina@users.noreply.github.com>
2024-05-16 22:42:33 +0100

    added responsive navbar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//NAVBAR
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none';
+}
 
 // scroll up button
 
